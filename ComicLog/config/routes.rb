391f79513ad34cfd2d5b82a69ea72df7cd747617
Tony Turetsky <TTuretsky@gmail.com>
2018-07-30 16:13:44 -0400

    added gemfile for comicvine api

--- ComicLog/config/routes.rb
+++ ComicLog/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :user_issues
   resources :publishers
   resources :issues
   resources :comics
-  resources :user_comics
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
