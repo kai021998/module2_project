5dabcfaa1e3dcf040d9b40234e797d31c26eb411
Tony Turetsky <TTuretsky@gmail.com>
2018-08-02 09:40:12 -0400

    builded more show pages for coimcs

--- ComicLog/app/controllers/comics_controller.rb
+++ ComicLog/app/controllers/comics_controller.rb
@@ -1,2 +1,29 @@
 class ComicsController < ApplicationController
+
+  def index
+    @comics = Comic.all
+    render :index
+  end
+
+  def new
+    @comic = Comic.new
+  end
+
+  def create
+    @comic = Comic.new(comic_params)
+    if @comic.valid?
+      @comic.save
+      flash[:notice] = "You created a comic!"
+      redirect_to comics_path
+    else
+      redirect_to new_comic_path
+    end
+  end
+
+private
+
+  def comic_params
+    params.require(:comic).permit(:title, :publisher_id)
+  end
+
 end
