8c249b9840c6a23300ebec1f4d0b73e1c89245c5
kai021998 <kai.han@balliol.ox.ac.uk>
2018-08-02 16:01:03 -0400

    added publisher stuff

--- ComicLog/app/controllers/publishers_controller.rb
+++ ComicLog/app/controllers/publishers_controller.rb
@@ -1,6 +1,12 @@
 class PublishersController < ApplicationController
 
   def index
+    @publishers = Publisher.all
   end
-  
+
+  def show
+    @publisher = Publisher.find(params[:id])
+  end
+
+
 end
